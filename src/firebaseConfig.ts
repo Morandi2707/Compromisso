fa2b577260d47602e5672ed73981e0c2184ec786
Eduardo Morandi <eduardo.morandi@BNGMETAL.CORP>
2025-02-17 16:00:06 -0300

    banco de dados concertado

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -13,14 +13,14 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASpsGeogr3kc4Ynr-UhHUbWVT9ZpoGOzQ",
-  authDomain: "compromisso-105d4.firebaseapp.com",
-  projectId: "compromisso-105d4.firebasestorage.app",
-  storageBucket: "288624322555",
-  messagingSenderId: "1:288624322555:web:695ce7706b22adb9e0cd5e",
-  appId: "G-8BHJ09M6S1"
-};
-
+    apiKey: "AIzaSyASpsGeogr3kc4Ynr-UhHUbWVT9ZpoGOzQ",
+    authDomain: "compromisso-105d4.firebaseapp.com",
+    projectId: "compromisso-105d4",
+    storageBucket: "compromisso-105d4.appspot.com", // Corrigido aqui
+    messagingSenderId: "288624322555",
+    appId: "1:288624322555:web:695ce7706b22adb9e0cd5e",
+    measurementId: "G-8BHJ09M6S1" // Esse é o valor correto para o measurementId
+  };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
